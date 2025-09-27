f01695f4e716d5ef16d367b5178e650537fece00
Javo <erfiaco@gmail.com>
2025-09-28 00:20:46 +0200

    empiezo a anadir oled

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ from gpiozero import Button
 from threading import Event, Thread
 import LCD_I2C_classe as LCD
 lcd = LCD.LCD_I2C()
+import oled.clase
 
 # Force gpiozero to use RPi.GPIO
 os.environ["GPIOZERO_PIN_FACTORY"] = "rpigpio"
