03e7316af20b2296434c7cb42f0620a3d26d1f34
Javo <erfiaco@gmail.com>
2025-11-14 21:54:22 +0100

    hacemos pruebas con pines, funciona pigpio, no funciona gpiozero buttons

--- software/buttons_manager.py
+++ software/buttons_manager.py
@@ -4,10 +4,10 @@ os.environ["GPIOZERO_PIN_FACTORY"] = "pigpio"#"rpigpio"
 
 class ButtonsManager:
     def __init__(self, on_grabar_press, on_mute_press, on_play_press, on_stop_press, on_long_stop):
-        self.btn_grabar = Button(26)
-        self.btn_mute = Button(6)
-        self.btn_play = Button(13)
-        self.btn_stop = Button(19, hold_time=3.0)  # ← Configura hold a 3s
+        self.btn_grabar = Button(19)
+        self.btn_mute = Button(13)
+        self.btn_play = Button(6)
+        self.btn_stop = Button(26, hold_time=3.0)  # ← Configura hold a 3s
 
         self.on_grabar_press = on_grabar_press
         self.on_mute_press = on_mute_press
