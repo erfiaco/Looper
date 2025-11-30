cdd017e6ead17a767ebe993f31cbf691d3bb930e
Javo <erfiaco@gmail.com>
2025-11-30 14:52:50 +0100

    modificamos el apagadogpiozero, que NO ESTAMOS USANDO porque Gpiozero creo que funciona cn lgpio, y colisionan las asignaciones de looper y de este programa. entones al final usamos shutdown  que no usa gpio, sino que usa directamente el sistema default de linux

--- libs/apagado_gpiozero.py
+++ libs/apagado_gpiozero.py
@@ -2,14 +2,17 @@
 from gpiozero import Button
 from signal import pause
 import os
+import subprocess
 
-button = Button(5, pull_up=True, hold_time=2)  # hold_time = segundos para long press
+# Mismo pin que tu boton STOP del looper
+btn = Button(5, pull_up=True, hold_time=2.5)  # 2.5 segundos para no solaparse con el looper, actualmente usa el primer boton grabacion
 
-def apagar():
-    print("Apagando la Raspberry!")
-    os.system("sudo shutdown -h now")
+def shutdown():
+    print("BOTon STOP MANTENIDO 2.5s, APAGANDO LA RASPBERRY PI")
+    # Opcional: puedes emitir un sonido, apagar LEDs, etc.
+    subprocess.run(["sudo", "shutdown", "-h", "now"])
 
-button.when_held = apagar
+btn.when_held = shutdown
 
-print("Boton de apagado activo. Manten ulsado 2s para apagar.")
-pause()  # espera eternamente a eventos
+print("Servicio de apagado fisico activo (GPIO 5 - hold 2.5s)")
+pause()
